3e78b92e3e020945f96e015afb3b6ca47ba9b0b1
Thiago Franchin <thiago.franchin@gmail.com>
2019-03-04 13:34:50 -0300

    Create Button

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,12 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <b-button variant="info">Info</b-button>
+    <p>Você quer me ver ...</p>
+    <p>...ou eu</p>
+    <p>Você quer me ver ...</p>
+    <p>...ou eu</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Listing",
   data() {
-    return {};
+    return {
+      livro: {
+        titulo: "O Senhor dos Anéis",
+        autor: "J.R.R. Tolkiens",
+        Volume: "3"
+      },
+      aluno: {
+        id: 10,
+        nome: "Thiago",
+        notas: [10.0, 9.98, 11, 9.99]
+      }
+    };
   }
 };
 </script>
